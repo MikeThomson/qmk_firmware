1857857c2af088517ccf438304522b7b1f0c78fd
mike <mike@bluetempest.net>
2020-01-29 12:32:01 -0600

    Add salmon / teal double knight rider animation

--- tmk_core/protocol/arm_atsam/led_matrix_programs.c
+++ tmk_core/protocol/arm_atsam/led_matrix_programs.c
@@ -92,7 +92,26 @@ led_setup_t leds_rainbow_s[] = {
 // The last entry must be { .end = 1 }
 // Add the new animation name to the list below following its format
 
-void *led_setups[] = {leds_rainbow_s, leds_rainbow_ns, leds_teal_salmon, leds_yellow, leds_red, leds_green, leds_blue, leds_white, leds_white_with_red_stripe, leds_black_with_red_stripe, leds_off};
+
+
+// @TODO MIKE: figure out a better way to allow this in keymap.c (or other user overwritable place). maybe add something
+// external and combine the two collections?
+// @TODO this also has a weird effect near the middle because of the double white override, do it better
+led_setup_t leds_salmon_knight_rider[] = {
+    {.hs = 0, .he = 100, .rs = 24, .re = 24, .gs = 215, .ge = 215, .bs = 204, .be = 204, .ef = EF_NONE}, // teal background
+
+    {.hs = 0, .he = 30, .rs = 255, .re = 255, .gs = 255, .ge = 255, .bs = 255, .be = 255, .ef = EF_SCR_R | EF_OVER}, // white canvas for salmon
+    {.hs = 0, .he = 15, .rs = 231, .re = 0, .gs = 40, .ge = 141, .bs = 51, .be = 137, .ef = EF_SCR_R | EF_SUBTRACT}, // fade up
+    {.hs = 15, .he = 30, .rs = 0, .re = 231, .gs = 141, .ge = 40, .bs = 137, .be = 51, .ef = EF_SCR_R | EF_SUBTRACT}, // fade down
+
+    {.hs = 70, .he = 100, .rs = 255, .re = 255, .gs = 255, .ge = 255, .bs = 255, .be = 255, .ef = EF_SCR_L | EF_OVER}, // white canvas for salmon
+    {.hs = 70, .he = 85, .rs = 231, .re = 0, .gs = 40, .ge = 141, .bs = 51, .be = 137, .ef = EF_SCR_L | EF_SUBTRACT}, // fade up
+    {.hs = 85, .he = 100, .rs = 0, .re = 231, .gs = 141, .ge = 40, .bs = 137, .be = 51, .ef = EF_SCR_L | EF_SUBTRACT}, // fade down
+
+    {.end = 1},
+};
+
+void *led_setups[] = {leds_rainbow_s, leds_rainbow_ns, leds_teal_salmon, leds_yellow, leds_red, leds_green, leds_blue, leds_white, leds_white_with_red_stripe, leds_black_with_red_stripe, leds_salmon_knight_rider, leds_off};
 
 const uint8_t led_setups_count = sizeof(led_setups) / sizeof(led_setups[0]);
 
