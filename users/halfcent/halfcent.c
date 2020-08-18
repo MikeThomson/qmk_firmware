22876ed2872559f681d998323c346147e07dc08b
mike <mike@bluetempest.net>
2020-08-17 23:19:25 -0500

    add sidecar

--- users/halfcent/halfcent.c
+++ users/halfcent/halfcent.c
@@ -6,6 +6,6 @@ layer_state_t layer_state_set_keymap (layer_state_t state) {
 }
 
 layer_state_t layer_state_set_user (layer_state_t state) {
-    state = update_tri_layer_state(state, 1, 2, 3);
+//    state = update_tri_layer_state(state, 1, 2, 3);
     return layer_state_set_keymap (state);
 }
